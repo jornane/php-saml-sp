07379c0fa13d2cf5217c3cf6a54ba22c361a3fbf
François Kooman <fkooman@tuxed.net>
2019-02-25 13:27:08 +0100

    fix benchmarks

--- benchmarks/ResponseBench.php
+++ benchmarks/ResponseBench.php
@@ -36,13 +36,13 @@ class ResponseBench
      */
     public function benchHandleResponse()
     {
-        $response = new Response(new DateTime('2019-01-02T20:05:33Z'));
+        $response = new Response(new DateTime('2019-02-23T17:04:21Z'));
         $samlResponse = \file_get_contents(\dirname(__DIR__).'/tests/data/assertion/FrkoIdP.xml');
         $samlAssertion = $response->verify(
             $samlResponse,
-            'http://localhost:8081/metadata.php',
-            '_6f4ccd6d1ced9e0f5ac6333893c64a2010487d289044b6bb4497b716ebc0a067',
-            'http://localhost:8081/acs.php',
+            'http://localhost:8081/metadata',
+            '_2483d0b8847ccaa5edf203dad685f860',
+            'http://localhost:8081/acs',
             [],
             new IdpInfo('http://localhost:8080/metadata.php', 'http://localhost:8080/sso.php', null, [PublicKey::fromFile(\dirname(__DIR__).'/tests/data/certs/FrkoIdP.crt')])
         );
